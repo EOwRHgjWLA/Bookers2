0e24bd1aaddfea7cccffc280d78783e5372ff16a
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-11-20 08:08:52 +0000

    [Finish]Rspecテスト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :books
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
@@ -16,6 +19,6 @@ class User < ApplicationRecord
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
-  has_many :book, dependent: :destroy
+
 
 end
